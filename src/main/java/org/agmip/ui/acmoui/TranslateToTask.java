b9dbbbc8d74226896ec3a4d5abec93d2ba0b3a06
MengZhang <meng.zhang15@ufl.edu>
2013-02-07 12:58:40 -0600

    1. Add Apsim spot 2. Change to use radio button to select data source 3. Disable multiple thread output

--- src/main/java/org/agmip/ui/acmoui/TranslateToTask.java
+++ src/main/java/org/agmip/ui/acmoui/TranslateToTask.java
@@ -1,10 +1,7 @@
 package org.agmip.ui.acmoui;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import org.agmip.core.types.TranslatorOutput;
 import org.agmip.translators.acmo.AcmoDssatCsvOutput;
 import org.apache.pivot.util.concurrent.Task;
@@ -15,44 +12,57 @@ import org.slf4j.LoggerFactory;
 public class TranslateToTask extends Task<String> {
 
     private HashMap data;
-    private ArrayList<String> translateList;
+//    private ArrayList<String> translateList;
+    private String translateTpye;
     private String destDirectory;
     private static Logger LOG = LoggerFactory.getLogger(TranslateToTask.class);
 
-    public TranslateToTask(ArrayList<String> translateList, HashMap data, String destDirectory) {
+    public TranslateToTask(String translateTpye, HashMap data, String destDirectory) {
         this.data = data;
         this.destDirectory = destDirectory;
-        this.translateList = translateList;
-//        for (String trType : translateList) {
-//            if (!trType.equals("JSON")) {
-//                this.translateList.add(trType);
-//            }
-//        }
+        this.translateTpye = translateTpye;
+//        this.translateList = translateList;
     }
 
     @Override
     public String execute() throws TaskExecutionException {
-        ExecutorService executor = Executors.newFixedThreadPool(64);
-        TranslatorOutput[] translators = new TranslatorOutput[translateList.size()];
+//        ExecutorService executor = Executors.newFixedThreadPool(64);
+        TranslatorOutput translator;
         File output;
+
         try {
-            for (int i = 0; i < translateList.size(); i++) {
-                String tr = translateList.get(i);
-                if (tr.equals("DSSAT")) {
-                    translators[i] = new AcmoDssatCsvOutput();
-                    String destination = destDirectory; //+ File.separator + tr;
-                    LOG.debug("Translating with :" + translators[i].getClass().getName());
-                    Runnable thread = new TranslateRunner(translators[i], data, destination);
-                    executor.execute(thread);
-                } else if (tr.equals("APSIM")) {
-                } else {
-                }
+
+            // Run select mode
+//            for (int i = 0; i < translateList.size(); i++) {
+//                String tr = translateList.get(i);
+            if (translateTpye.equals("DSSAT")) {
+                translator = new AcmoDssatCsvOutput();
+                LOG.debug("Translating with :" + translator.getClass().getName());
+//                Runnable thread = new TranslateRunner(translator, data, destination);
+//                executor.execute(thread);
+            } else if (translateTpye.equals("APSIM")) {
+                translator = null;
+            } else {
+                translator = null;
             }
-            executor.shutdown();
-            while (!executor.isTerminated()) {
+            translator.writeFile(destDirectory, data);
+//            }
+
+            // Release executor
+//            executor.shutdown();
+//            while (!executor.isTerminated()) {
+//            }
+//            executor = null;
+            if (translator == null) {
+                return "";
+            } else if (translateTpye.equals("DSSAT")) {
+                output = ((AcmoDssatCsvOutput) translator).getOutputFile();
+            } else if (translateTpye.equals("APSIM")) {
+                output = null; //TODO
+            } else {
+                output = null;
             }
-            executor = null;
-            output = ((AcmoDssatCsvOutput) translators[0]).getOutputFile(); // TODO
+
         } catch (Exception ex) {
             throw new TaskExecutionException(ex);
         }
